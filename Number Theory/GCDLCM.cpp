26c5040d2481f01afbad5905d423638193ce010b
KH <dlrudgns1990@gmail.com>
2021-02-24 17:51:47 +0900

    [LeeKyungHun, dlrudgns1990@gmail.com] [제목] 최대공약수/최소 공배수 소스코드 추가 [수정내용] 최대공약수/최소 공배수 소스코드 추가

---
+++ Number Theory/GCDLCM.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+// 최대 공약수를 구하는 함수
+// 해당 함수를 통해 최대 공약수를 return
+// 유클리드 호제법을 이용한 로직
+int GCD(int a, int b)
+{
+	int temp = 0;
+	while (b != 0) 
+	{
+		temp = a % b;
+		a = b;
+		b = temp;
+	}
+
+	return a;
+}
+
+int main() {
+	int inputA = 0;
+	int inputB = 0;
+
+	int resultGCD = 0;
+	int resultLCM = 0;
+
+	scanf("%d %d", &inputA, &inputB);
+
+
+	resultGCD = GCD(inputA, inputB);
+	resultLCM = (inputA * inputB) / resultGCD;
+
+	printf("%d, %d", resultGCD, resultLCM);
+
+	return 0;
+}
